4c6153ffbef955498864bdd5de751abbb86286d3
Charles Love Winters III <charlie.winters17@gmail.com>
2021-11-04 21:32:43 -0500

    Add magic method decorator to fix intellisense bug

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,6 +11,8 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * 
+ * @method string getUserIdentifier()
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
